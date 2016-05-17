6062467550428754d1fe478654ff6c71c99408ed
KingOfBraves <chadifferus@gmail.com>
2016-05-17 19:08:00 -0300

    added chest tile

---
+++ TombstoneDeathMod/Tiles/TombstoneChest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TombstoneDeathMod.Tiles
+{
+    class TombstoneChest : ModTile
+    {
+
+    }
+}
